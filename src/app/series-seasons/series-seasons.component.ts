2b6d3ab332588dc5b890452675af16271a7b2ff4
Pau <byprokiller2@gmail.com>
2024-01-13 20:01:29 +0100

    feat: Add SeriesDetail details

--- src/app/series-seasons/series-seasons.component.ts
+++ src/app/series-seasons/series-seasons.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { SeriesSeason, SeriesSeasonDetail, SeriesSeasonDetailEpisode } from '../interfaces/series/seriesDetail.interface';
 import { SeriesService } from '../series.service';
 import { Serie } from '../interfaces/series/series.interface';
-import { Genre } from '../interfaces/common/common.interfaces';
+import { CreditsResponseItem, Genre, WatchProvidersResultsItem } from '../interfaces/common/common.interfaces';
 
 @Component({
   selector: 'app-series-seasons',
@@ -13,10 +13,18 @@ export class SeriesSeasonsComponent implements OnInit {
   @Input() seasonsCount = 0;
   @Input() seasons: SeriesSeason[] | undefined;
   @Input() seriesId: number = 0;
+  @Input() seriesName: string | undefined;
 
   seasonNumber: number | undefined;
   seasonDetail: SeriesSeasonDetail | undefined;
   seasonEpisodes: SeriesSeasonDetailEpisode[] = [];
+
+  seriesCast: CreditsResponseItem[] | undefined;
+  seriesCrew: CreditsResponseItem[] | undefined;
+
+  rentWatchProviders: WatchProvidersResultsItem[] | undefined;
+  buyWatchProviders: WatchProvidersResultsItem[] | undefined;
+  watchProvidersLink: string | undefined;
 
   relatedSeries: Serie[] = [];
   genreList: Genre[] = [];
@@ -32,8 +40,10 @@ export class SeriesSeasonsComponent implements OnInit {
   ngOnInit(): void {
     let seasonNumber = 1;
 
-    if (this.seasons)
-      seasonNumber = this.seasons[0].season_number;
+    if (this.seasons) {
+      let firstSeason = this.seasons[0];
+      seasonNumber = firstSeason.season_number > 0 ? firstSeason.season_number : 1;
+    }
 
     this.seriesService.getSeriesSeasonDetail(this.seriesId, seasonNumber).subscribe((response) => {
       this.seasonDetail = response;
@@ -46,6 +56,32 @@ export class SeriesSeasonsComponent implements OnInit {
 
     this.seriesService.getGenresList().subscribe((response) => {
       this.genreList = response.genres;
+    });
+
+    this.seriesService.getSeriesCredits(this.seriesId).subscribe((response) => {
+      let seriesCast = this.removeDuplicates<CreditsResponseItem>(response.cast).filter(item => item.profile_path);
+
+      if (seriesCast.length > 0)
+        this.seriesCast = seriesCast;
+
+      let seriesCrew = this.removeDuplicates<CreditsResponseItem>(response.crew).filter(item => item.profile_path);
+
+      if (seriesCrew.length > 0)
+        this.seriesCrew = seriesCrew;
+    });
+
+    this.seriesService.getSeriesWatchProviders(this.seriesId).subscribe((response) => {
+      let rent = response.results?.GB?.rent;
+
+      if (rent && rent.length > 0)
+        this.rentWatchProviders = rent;
+
+      let buy = response.results?.GB?.buy;
+
+      if (buy && buy.length > 0)
+        this.buyWatchProviders = buy;
+
+      this.watchProvidersLink = response.results?.GB?.link;
     });
   }
 
@@ -75,5 +111,21 @@ export class SeriesSeasonsComponent implements OnInit {
     this.actionState.episodes = false;
     this.actionState.related = false;
     this.actionState.details = true;
+  }
+
+  private removeDuplicates<T>(array: { id: number }[]): T[] {
+    let filtered: T[] = [];
+    let ids: number[] = [];
+
+    for (let i = 0; i < array.length; i++) {
+      let item = array[i];
+
+      if (!ids.includes(item.id)) {
+        filtered.push(item as T);
+        ids.push(item.id);
+      }
+    }
+
+    return filtered;
   }
 }
